51d599ed21770e806c4628f7b182a4838a54a38c
torman <torman_cheng@yahoo.com>
2013-10-20 10:03:32 -0400

    add css js file; add login and signup view; add .gitignore

--- config/config.php
+++ config/config.php
@@ -31,7 +31,7 @@ A email designated to receive messages from the server. Examples:
 	define('SYSTEM_EMAIL', 'webmaster@myapp.com'); 
 
 # Default DB name for this app
-	define('DB_NAME', "myapp_com"); 
+	define('DB_NAME', "p2_torman_us"); 
 
 # Timezone
 	define('TIMEZONE', 'America/New_York');
@@ -43,5 +43,5 @@ A email designated to receive messages from the server. Examples:
 	//define('SMTP_PASSWORD', '');
 
 # For extra security, you might want to set different salts than what the core uses
-	//define('PASSWORD_SALT', '');
-	//define('TOKEN_SALT', '');
+	//define('PASSWORD_SALT', 'afdfasfa;fajfnsadeiasoasfal');
+	//define('TOKEN_SALT', 'icantypewhateveriliketherenobodyshouldcomeheretoread');
--- controllers/c_users.php
+++ controllers/c_users.php
@@ -4,7 +4,7 @@ class  users_controller extends base_controller {
 	public function __construct() {
 
 		parent::__construct();
-		echo "users_controller construct called<br><br>";
+		// echo "users_controller construct called<br><br>";
 	}
 
 	public function profile ($user_name = NULL) {
@@ -23,4 +23,92 @@ class  users_controller extends base_controller {
 
 		echo $this->template;
 	}
+
+	public function signup() {
+		
+		$this->template->content = View::instance('v_users_signup');
+		$this->template->title = "Sign Up";
+
+		echo $this->template;
+
+	}
+
+	public function p_signup() {
+
+/*
+		echo '<pre>';
+		print_r($_POST);
+		echo '<pre>';
+*/
+		# we want to know when the user is created and user's data is changed
+		$_POST['created'] = Time::now();
+		$_POST['modified'] = Time::now();
+
+		/* encrypt user's password */
+		$_POST['password'] = sha1(PASSWORD_SALT.$_POST['password']);
+
+		# create an encrypted token via email address and a arandom string 
+		$_POST['token'] = sha1(TOKEN_SALT.$_POST['email'].Utils::generate_random_string());
+
+
+		# insert the user into database
+		$user_id = DB::instance(DB_NAME)->insert('users', $_POST);
+
+		# want to see the output; should comment out later
+		echo "user ID is $user_id";
+		echo "You're signed up";
+	}
+
+	public function login() {
+
+		$this->template = View::instance('v_users_login');
+		$this->template->title = "Login";
+
+		echo $this->template;
+		
+	}
+
+	public function p_login() {
+		
+		# Sanitize the user input from login form
+		$_POST = DB::instance(DB_NAME)->sanitize($_POST);
+
+		# hash the password submitted by the user
+		$_POST['password'] = sha1(PASSWORD_SALT.$_POST['password']);
+
+		# retrieve the token from database
+
+		$q = "SELECT token FROM users WHERE email = '"  
+				. $_POST['email'] . "'"
+				. " AND "
+				. "password = '" 
+				. $_POST['password'] . "'"; 
+
+
+		$token = DB::instance(DB_NAME)->select_field($q);
+		#echo $token;
+
+		# if no token found (wrong email or password), send the user to login page. 
+		if (!$token) {
+			
+			Router::redirect("/users/login");
+
+		} 
+		else {
+
+			setcookie('token', $token, strtotime('+2 week'), '/');
+			
+			# send the user to home page now
+			Router::redirect("/");
+		}
+/*
+		
+		$q = "SELECT token FROM users 
+				WHERE email = '".$_POST['email']."'
+				AND password = ".$_POST['password']."'";
+
+		echo $q;
+*/
+		}
+
 }
